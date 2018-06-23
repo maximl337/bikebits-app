6f1b984f62d4541b7f1430c976a710ed9e68bbe8
Angad Dubey <angad_dubey@hotmail.com>
2018-06-23 12:59:02 -0400

    Add migration and models

---
+++ database/migrations/2018_06_23_161350_create_journey_objects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJourneyObjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journey_objects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('object_url');
+            $table->integer('object_type_id')->unsigned();
+            $table->foreign('object_type_id')->references('id')->on('journey_object_types')->onDelete('cascade');
+            $table->integer('journey_id')->unsigned();
+            $table->foreign('journey_id')->references('id')->on('journeys')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journey_objects');
+    }
+}
