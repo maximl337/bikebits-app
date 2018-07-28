165ce81549d3eba73c934301556e8a9ce857cec5
Angad Dubey <angad_dubey@hotmail.com>
2018-07-28 10:55:23 -0400

    stuff

--- public/js/app.js
+++ public/js/app.js
@@ -48414,7 +48414,7 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "row" }, [_vm._v("\n  journey lane\n")])
+  return _c("div", { staticClass: "row" })
 }
 var staticRenderFns = []
 render._withStripped = true
